56ad5e2ec8f20e3f68add78e495201ecf7c58896
Kim Rune Møller <kimrune@hotmail.no>
2024-06-01 12:00:36 +0200

    adds images remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["cdn.sanity.io", "via.placeholder.com"]
+    domains: ["cdn.sanity.io", "via.placeholder.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: ""
+      },
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
+        port: ""
+      }
+    ]
   }
 };
 

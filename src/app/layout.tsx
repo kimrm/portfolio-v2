cb8ef06a29e8d71938073cc4bfb4ab5a43563887
Kim Rune Møller <kimrune@hotmail.no>
2024-06-01 12:01:27 +0200

    refactor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Calistoga } from "next/font/google";
 import "./globals.css";
 import "@fontsource/calistoga";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";

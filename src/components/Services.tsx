795a0c9712645e1a666cef8a2f173b320fb21fd0
Kim Rune Møller <kimrune@hotmail.no>
2024-06-01 19:41:09 +0200

    text change

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -4,13 +4,19 @@ export default function Services() {
   return (
     <div>
       <h2 className="font-bold uppercase tracking-wide text-gray-300 mb-5">
-        Frontend with React, or fullstack with Next.js
+        Services I offer
       </h2>
       <p className="max-w-prose leading-relaxed">
         Landingpage for your business or product? Webshop integrations? A
-        homepage for your organization? I create nice and user-friendly websites
-        and apps with the newest technology and standards.
+        homepage for your organization? I create user-friendly websites and apps
+        with the newest technology and standards. I can help you with:
       </p>
+      <ul className=" list-disc px-4 mt-3">
+        <li>Web development</li>
+        <li>SEO optimization</li>
+        <li>Performance optimization</li>
+        <li>Web hosting</li>
+      </ul>
     </div>
   );
 }

49d4b5a90856dd404f523fa3218bcf8fd1d8d16f
Kim Rune Møller <kimrune@hotmail.no>
2024-06-01 18:08:11 +0200

    removes opacity

--- src/components/ui/MovingBorder.tsx
+++ src/components/ui/MovingBorder.tsx
@@ -56,7 +56,7 @@ export function BorderContainer({
 
       <div
         className={cn(
-          "relative bg-slate-900/[0.9] border border-slate-800 backdrop-blur-xl text-white flex items-center justify-center w-full h-full text-sm antialiased",
+          "relative bg-slate-900 border border-slate-800 backdrop-blur-xl text-white flex items-center justify-center w-full h-full text-sm antialiased",
           className
         )}
         style={{

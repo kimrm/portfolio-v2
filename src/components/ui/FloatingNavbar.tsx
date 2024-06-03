d0421ed5a950a63f2dd39ddb330da1a6479dc538
Kim Rune Møller <kimrune@hotmail.no>
2024-06-04 01:01:43 +0200

    accessibility adjustments

--- src/components/ui/FloatingNavbar.tsx
+++ src/components/ui/FloatingNavbar.tsx
@@ -64,6 +64,7 @@ export const FloatingNav = ({
           <Link
             key={`link=${idx}`}
             href={navItem.link}
+            title={navItem.name}
             className={cn(
               "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500 uppercase"
             )}

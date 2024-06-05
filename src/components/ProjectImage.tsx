66df16248d409722ad78c58fa2994fdb3c9f2de3
Kim Rune Møller <kimrune@hotmail.no>
2024-06-05 14:21:34 +0200

    fix next prev buttons on image

--- src/components/ProjectImage.tsx
+++ src/components/ProjectImage.tsx
@@ -14,22 +14,48 @@ export default function ProjectImage({ images }: ProjectImageProps) {
       {images.length > 1 && (
         <>
           <button
-            className="absolute top-0 w-1/2 h-full right-0 p-2 text-white bg-transparent"
+            className="absolute top-0 w-1/2 flex items-center justify-end h-full right-0 p-2 text-white"
             onClick={() =>
               setCurrentImageIndex((prev) => (prev + 1) % images.length)
             }
           >
-            Next
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m8.25 4.5 7.5 7.5-7.5 7.5"
+              />
+            </svg>
           </button>
           <button
-            className="absolute top-0 w-1/2 h-full left-0 p-2 text-white bg-transparent"
+            className="absolute top-0 w-1/2 h-full left-0 p-2 text-white flex items-center justify-start"
             onClick={() =>
               setCurrentImageIndex(
                 (prev) => (prev - 1 + images.length) % images.length
               )
             }
           >
-            Previous
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 19.5 8.25 12l7.5-7.5"
+              />
+            </svg>
           </button>
         </>
       )}

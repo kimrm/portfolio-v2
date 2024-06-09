fca9f9dff8b8b969e017b94d5ad445260b09bc4a
Kim Rune Møller <kimrune@hotmail.no>
2024-06-09 12:55:27 +0200

    adds className prop

--- src/components/ui/MovingHeading.tsx
+++ src/components/ui/MovingHeading.tsx
@@ -4,16 +4,18 @@ import React from "react";
 import { motion } from "framer-motion";
 
 export default function MovingHeading({
-  children
+  children,
+  className
 }: {
   children: React.ReactNode;
+  className?: string;
 }) {
   return (
     <motion.h2
       initial={{ x: 25 }}
       animate={{ x: -25 }}
       transition={{ duration: 10, repeat: Infinity, repeatType: "reverse" }}
-      className="text-4xl italic md:text-5xl lg:text-8xl mb-2 font-display text-sky-200"
+      className={className}
     >
       {children}
     </motion.h2>
